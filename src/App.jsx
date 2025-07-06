542ba937c20065667978ed46dce4b9fe14fbbea6
HieuDoanThe <Thehieu0814@gmail.com>
2025-07-06 22:12:21 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ThesisList from "./pages/thesis-fe";
+
+function App() {
+  return (
+    <div className="App">
+      <ThesisList />  
+    </div>
+  );
+}
+
+export default App;
